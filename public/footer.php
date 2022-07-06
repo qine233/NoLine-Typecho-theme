968a62535109049c241ca779926251f5a6734c18
qine <1351856278@qq.com>
2022-07-06 16:07:57 +0800

    Add files via upload
    添加次container的ajax加载

--- public/footer.php
+++ public/footer.php
@@ -1,3 +1,27 @@
+
 <footer class="footer">
+
 <h3 class="text-ellipsis">&copy;2021 Copyright&nbsp;&nbsp;<a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?></a></h3>
 </footer>
+<script src="<?php $this->options->themeUrl('css/jquery.min.js'); ?>"></script>
+<script src="<?php $this->options->themeUrl('css/pjax.js'); ?>"></script>
+<script>
+
+
+    $(document).pjax('a[href^="<?php Helper::options()->siteUrl()?>"]:not(a[target="_blank"], a[no-pjax])', {
+        container: '#pjax-container',
+        fragment: '#pjax-container',
+        timeout: 8000
+    }).on('pjax:send',
+        function() {
+            NProgress.start();
+
+        }).on('pjax:complete',
+        function() {
+            NProgress.done();
+            // setupContents();
+            // reHighlightCodeBlock();//代码高亮函数重载
+
+        })
+
+</script>
--- public/header.php
+++ public/header.php
@@ -8,12 +8,14 @@
 <script src="https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.3.1/build/languages/go.min.js"></script>
 
 <script src="<?php $this->options->themeUrl('css/viewer.js'); ?>"></script>
+    <script src="<?php $this->options->themeUrl('css/nprogress.js'); ?>"></script>
 <title><?php $this->archiveTitle(array('category' => '分类 %s 下的文章', 'search' => '包含关键字 %s 的文章', 'tag' => '标签 %s 下的文章', 'author' => '%s 发布的文章'), '', ' - '); ?><?php $this->options->title(); ?></title>
 <link rel="stylesheet" href="<?php $this->options->themeUrl('css/bootstrap.min.css');  ?>">
 <link rel="stylesheet" href="<?php $this->options->themeUrl('css/Q-style.css'); ?>">
 <link rel="stylesheet" href="<?php $this->options->themeUrl('css/post.css'); ?>">
 <link rel="stylesheet" href="<?php $this->options->themeUrl('css/mobile.css');  ?>">
 <link rel="stylesheet" href="<?php $this->options->themeUrl('css/viewer.css');  ?>">
+    <link rel="stylesheet" href="<?php $this->options->themeUrl('css/nprogress.css');  ?>">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.3.1/build/styles/atom-one-dark.min.css">
 
 <?php $this->header(); ?>
@@ -41,4 +43,4 @@
 
           
         </div>
-     
+<div id="pjax-container">
