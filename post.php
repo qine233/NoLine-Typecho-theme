2a5da5e975e19342e0c9c6b24bdbbdcdcf077d21
qine <1351856278@qq.com>
2022-09-11 22:41:21 +0800

    backed up

--- post.php
+++ post.php
@@ -3,30 +3,26 @@
 <?php $this->need('public/header.php'); ?>
 <div class="content-all"><?php $this->need('sidebar.php'); ?>
         <div class="content">
-      <?php $this->need('crumb.php'); ?>
-        <div class="typecho-header">  <span class="typecho-category"><?php $this->category('.')?></span><i class="bg-primary"></i>
-        <h1 class="post-title" itemprop="name headline"><?php $this->title() ?></h1>
-        <span class="typecho-time"><?php $this->date('Y/m/d'); ?></span>
 
 
-        </div>
         <div class="typecho-user-text">
              <div class="content-list-post">
                  <!-- 下面text部分 -->
 		<div class="typecho-text text-tream " >
-      
+
 
         </div>
        <div class="content-text-2 " id="hideImg">
+         <h1 class="post-title" itemprop="name headline"><?php $this->title() ?></h1>
         <?php $this->content(); ?>
 
-        <script >hljs.initHighlightingOnLoad();</script> 
+        <script >hljs.initHighlightingOnLoad();</script>
     </div>
-	</div>		
+	</div>
     </div> <?php $this->need('comments.php'); ?>
-    </div>  
- 
-    
+    </div>
+
+
     <script src="https://cdn.jsdelivr.net/npm/vditor/dist/index.min.js"></script>
               <script type="text/javascript">
                      var image = new Viewer(document.getElementById('hideImg'),{
@@ -34,11 +30,11 @@
                                      });
                      </script>
 
-           </div> 
+           </div>
 </div></div>
 
       <?php $this->need('public/footer.php'); ?>
-        
 
-		
+
+
 </body>
