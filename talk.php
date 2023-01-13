6a67e1fd9ed3d4f6c667973d2e9280c36e7b1e31
qine <1351856278@qq.com>
2023-01-13 16:57:30 +0800

    更新时光机页面---后续再修改细节

--- talk.php
+++ talk.php
@@ -11,91 +11,83 @@
 <?php $this->need('public/header.php'); ?>
 
 <div class="content-all content-all-post">
-<?php $this->need('sidebar.php'); ?>
-        <div class="typecho-user-text">
-             <!-- <div class="content-list"> -->
+    <?php $this->need('sidebar.php'); ?>
+    <div class="typecho-user-text">
+        <!-- <div class="content-list"> -->
 
 
-             <div class="content-list-post">
+        <div class="content-list-post">
 
-    <div id="comments-padding-talk"><div class="typecho-header">
-<!-- 判断设置是否允许对当前文章进行评论 -->
-<?php if($this->allow('comment')): ?>
+            <div id="comments-padding-talk">
+                <div class="typecho-header">
+                    <!-- 判断设置是否允许对当前文章进行评论 -->
+                    <?php if($this->allow('comment')): ?>
 
- <h4 id="response"><?php $this->commentsNum('「&nbsp&nbsp时光动态&nbsp&nbsp」'); ?></h4>
- <?php if($this->user->hasLogin()): ?>
- <!-- 输入表单开始 -->
- <form method="post" action="<?php $this->commentUrl() ?>" id="comment_form">
+                    <h4 id="response"><?php $this->commentsNum('「&nbsp&nbsp时光动态&nbsp&nbsp」'); ?></h4>
+                    <?php if($this->user->hasLogin()): ?>
+                    <!-- 输入表单开始 -->
+                    <form method="post" action="<?php $this->commentUrl() ?>" id="comment_form">
 
-     <!-- 如果当前用户已经登录 -->
-
-
-         <!-- 显示当前登录用户的用户名以及登出连接 -->
-         <p style="margin-left: 4rem;">快来发射今日动态吧
-        </p>
-
-     <!-- 若当前用户未登录 -->
+                        <!-- 如果当前用户已经登录 -->
 
 
+                        <!-- 显示当前登录用户的用户名以及登出连接 -->
+                        <p style="margin-left: 4rem; font-size: 1.33rem;">快来书写今日心情吧
+                        </p>
+
+                        <!-- 若当前用户未登录 -->
 
 
-     <!-- 输入要回复的内容 -->
-     <div class="talk-content-talk">
-     <p><textarea class="text" rows="10" cols="50" name="text"><?php $this->remember('text'); ?></textarea></p>
-  <!-- <p><textarea  type="text" name="text" size="35" value="<?php $this->remember('text'); ?>" /></p> -->
+                        <!-- 输入要回复的内容 -->
+                        <div class="talk-content-talk">
+                            <p><textarea class="text" rows="10" cols="50" name="text"><?php $this->
+                                remember('text'); ?></textarea></p>
+                            <!-- <p><textarea  type="text" name="text" size="35" value="<?php $this->remember('text'); ?>" /></p> -->
 
-  <p class="submit-align"><input type="submit" value="发射" class="submit-talk" /></p>
- </div>
- </form> <?php endif; ?>
-<?php endif; ?>
-    </div></div>
- <div class="talk-list">
-
-<ol style="  display: flex;
-   flex-wrap: wrap; flex-direction:column-reverse;     padding-bottom: 3rem;" id="comment_list_talk">
-    <?php $this->comments()->to($comments); ?>
-        <?php while($comments->next()): ?>
-	<li style="
-                   font-weight: 100;
-                   margin-top: 20px;
-                   min-height: 15rem;
-                   width: 100%;
-                   margin-right: 35px;
-                   list-style: none;
-                   border-radius: 0.5rem;
-                   background-color: rgb(255, 252, 252);
-
-                   border-width: 0.2px;
-                   padding: 0.8rem 0.8rem 0.8rem 2.2rem;
-                   border-left: 1.5px solid #55a7ff;
-                   border-top: 1.5px solid #55a7ff;
-                 box-shadow: 0 4px 10px rgb(42 113 138 / 5%), 0 2px 5px rgb(27 101 166 / 10%);
-" id="list-talk-list-talk <?php $comments->theId(); ?>">
-	    <div class="comment_data">
-                <!-- <?php echo $comments->sequence(); ?>.  -->
-                <strong><?php $comments->author(); ?></strong>
-                &nbsp; <?php $comments->date('Y-m-d H:i'); ?>
+                            <p class="submit-align"> <input type="submit" value="发送" class="submit-talk"/></p>
+                        </div>
+                    </form>
+                    <?php endif; ?>
+                    <?php endif; ?>
+                </div>
             </div>
-	    <div class="comment_body"><?php $comments->content(); ?></div>
-	</li>
+            <div class="talk-list">
+
+                <ol style="  display: flex;
+   flex-wrap: wrap;     flex-direction: column;     padding-bottom: 3rem; " id="comment_list_talk">
+                    <?php $this->comments()->to($comments); ?>
+                    <?php while($comments->next()): ?>
+                    <li id="list-talk-list-talk <?php $comments->theId(); ?>">
+                        <div class="comment_data">
+                            <!-- <?php echo $comments->sequence(); ?>.  -->
+                            <img id="talkIMG" src="<?php $this->options->logoCss(); ?>"/>
+
+                            <div class="comment_body">
+
+                                <div class="talkUserTitle"> <strong><?php $comments->author(); ?></strong>
+                                &nbsp; <?php $comments->date('Y-m-d H:i'); ?>
+                            </div>
+                            <?php $comments->content(); ?>
+                            </div>
+                        </div>
+                    </li>
 
 
-	<?php endwhile; ?>
-</ol>
-<!-- <?php $comments->pageNav('« 前一页', '后一页 »'); ?>  -->
- </div>
-</div>
-
-
-
-</div>
-
+                    <?php endwhile; ?>
+                </ol>
+                <!-- <?php $comments->pageNav('« 前一页', '后一页 »'); ?>  -->
+            </div>
         </div>
 
-        </div>
 
-		<?php $this->need('public/footer.php'); ?>
-		</div>
-			 </div>
+    </div>
+
+</div>
+
+</div>
+
+<?php $this->need('public/footer.php'); ?>
+</div>
+</div>
 
 </body>
